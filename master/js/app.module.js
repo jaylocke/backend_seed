969727f9aa03843875eaa1d7e1d25307928a9052
jaylocke <85739230@qq.com>
2016-08-01 18:32:57 +0800

    seed

---
+++ master/js/app.module.js
@@ -0,0 +1,37 @@
+/*!
+ * 
+ * Angle - Bootstrap Admin App + AngularJS
+ * 
+ * Version: 3.0.0
+ * Author: @themicon_co
+ * Website: http://themicon.co
+ * License: https://wrapbootstrap.com/help/licenses
+ * 
+ */
+
+// APP START
+// ----------------------------------- 
+
+(function() {
+    'use strict';
+
+    angular
+        .module('seed', [
+            'app.core',
+            'app.routes',
+            'app.sidebar',
+            'app.navsearch',
+            'app.preloader',
+            'app.loadingbar',
+            'app.translate',
+            'app.settings',
+            //'app.notify',
+            'app.bootstrapui',
+            'app.elements',
+            'app.panels',
+            'app.forms',
+            'app.tables',
+            'app.utils'
+        ]);
+})();
+
---
+++ master/js/modules/lazyload/lazyload.constants.js
@@ -0,0 +1,22 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('app.lazyload')
+        .constant('APP_REQUIRES', {
+          // jQuery based and standalone scripts
+          scripts: {
+            'modernizr':          ['vendor/modernizr/modernizr.js'],
+            'icons':              ['vendor/fontawesome/css/font-awesome.min.css',
+                                   'vendor/simple-line-icons/css/simple-line-icons.css']
+          },
+          // Angular based script (use the right module name)
+          modules: [
+            // {name: 'toaster', files: ['vendor/angularjs-toaster/toaster.js', 'vendor/angularjs-toaster/toaster.css']}
+              {name: 'xeditable',                 files: ['vendor/angular-xeditable/dist/js/xeditable.js',
+                                                          'vendor/angular-xeditable/dist/css/xeditable.css']}
+          ]
+        })
+        ;
+
+})();
